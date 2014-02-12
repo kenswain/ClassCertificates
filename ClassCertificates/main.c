1d8c5bebeacc974cda85ba7fb1ec5282b15a3402
Ken Swain <ken@kenswain.com>
2014-02-12 15:16:03 -0600

    Initial Commit of the ClassCertificates project for big nerd ranch

---
+++ ClassCertificates/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  ClassCertificates
+//
+//  Created by Ken Swain on 2/12/14.
+//  Copyright (c) 2014 Ken Swain. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
