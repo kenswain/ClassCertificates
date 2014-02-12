a8d8a72ec1cc9a5033c0723a3cedb2d4e77173c8
Ken Swain <ken@kenswain.com>
2014-02-12 15:25:39 -0600

    Completed the first part of this program to teach functions.

--- ClassCertificates/main.c
+++ ClassCertificates/main.c
@@ -8,11 +8,19 @@
 
 #include <stdio.h>
 
+void congratulateStudent(char *student, char *course, int numDays)
+{
+    printf("%s has done as much %s programming as I can fit into %d days.\n", student, course, numDays);
+}
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    printf("Hello, World!\n");
+    congratulateStudent("Kate", "Cocoa", 5);
+    congratulateStudent("Bo", "Objective-C", 2);
+    congratulateStudent("Mike", "Python", 5);
+    congratulateStudent("liz", "iOS", 5);
+    
     return 0;
 }
 
